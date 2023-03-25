9b2781cc0f9b4c2adb5196f5fb125ee2dcad76ed
ahmad hadizadeh <giv.m@yahoo.com>
2023-03-25 12:27:47 +0430

    1 datatype

---
+++ testing.py
@@ -0,0 +1,47 @@
+tem1 = input("Please Inter the Full Name :")
+print(tem1)
+
+x = 3
+print(type(x))
+y = float(input("please insert Number :"))
+print(type(y))
+z = float((input("Happy New years 1402")))
+print(type(z))
+
+list
+ls = ["banana", "limonad", 300, "ahmad", 4000]
+ls2 = ["ahmad", 1, 4, "nazi"]
+print(type(ls))
+print(ls)
+print(ls[1])
+
+
+x2 = ("hello", "ahmad", 2)
+print(type(x2))
+
+dic = {
+    "name": "ahmad",
+    "family": "hadizadeh",
+    "phone": "09124933232",
+    "address": "Amol-Iran",
+    "code": 24567
+}
+print(dic)
+print(dic.keys())
+print(type(dic))
+print(dic["phone"])
+print(dic.values())
+print(type(dic))
+
+x3 = {
+    "name",
+    "Locations",
+    "date"
+}
+
+print(type(x3))
+
+x4 = range(10)
+
+for i in x4:
+    print(x4[i])
